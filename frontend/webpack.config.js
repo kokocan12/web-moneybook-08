ee342d132e8b065a8f1d49efb1dbecdb4d1f970b
min9120 <a9120a@gmail.com>
2022-07-18 21:42:58 +0900

    chore: 이니셜 프로젝트

---
+++ frontend/webpack.config.js
@@ -0,0 +1,52 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __dirname = path.resolve();
+
+export default {
+  entry: './frontend/app.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png|svg|ico)?$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[hash].[ext]',
+              fallback: 'file-loader',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './frontend/public/index.html',
+    }),
+  ],
+};
